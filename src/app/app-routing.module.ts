ede52913bee155318b925008379eefacbd59b470
CesarAAR <cealalvarezr@ittepic.edu.mx>
2021-12-16 17:37:19 -0700

    Navegacion y pantalla principal

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { PantallaComponent } from './pages/pantalla/pantalla.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+//import { CommonModule } from '@angular/common';
+const routes: Routes = [
+  {path: 'principal', component: PrincipalComponent },
+  {path: 'opiniones', component: PantallaComponent},
+  {path: '**',pathMatch: 'full', redirectTo:'principal'}
+];
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,21 @@ import { AppComponent } from './app.component';
 import { CrearOpinionComponent } from './components/crear-opinion/crear-opinion.component';
 import { ListarOpinionesComponent } from './components/listar-opiniones/listar-opiniones.component';
 import { environment } from 'src/environments/environment';
+import {PrincipalComponent} from './pages/principal/principal.component';
+import { PantallaComponent } from './pages/pantalla/pantalla.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrearOpinionComponent,
-    ListarOpinionesComponent
+    ListarOpinionesComponent,
+    PrincipalComponent,
+    PantallaComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
